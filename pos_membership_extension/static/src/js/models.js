9b42feb6c4df865120b89cb7058e2e541e34449d
flaenen <f.laenen@dooit.io>
2025-03-13 14:22:50 +0100

    [IMP] pos_membership_extension: consider membership in shopping cart The sale of a product requiring membership is allowed if the membership product is in the shopping cart, even if the customer is not yet a member.

--- pos_membership_extension/static/src/js/models.js
+++ pos_membership_extension/static/src/js/models.js
@@ -15,24 +15,27 @@ odoo.define("pos_membership_extension.models", function (require) {
             /**
              * Return if it's allowed to sell the product to the partner.
              *
-             * @param {partner} partner to test. (can be undefined)
+             * @param {Order} order to test.
              * @returns {Boolean} True if the sell is allowed, false otherwise.
              */
-            get_membership_allowed(partner) {
+            get_membership_allowed(order) {
                 // No categories means no restriction
                 if (this.allowed_membership_category_ids.length === 0) {
                     return true;
                 }
-                // If categories are set, but no partner, sell is forbidden in any case
-                if (!partner) {
-                    return false;
-                }
-                var common_categories = this.allowed_membership_category_ids.filter(
-                    function (categ) {
-                        return partner.membership_category_ids.indexOf(categ) !== -1;
-                    }
-                );
-                return common_categories.length !== 0;
+                var partner = order.partner;
+                // Checks that the partner has a membership category in common with those allowed or
+                // that one of the corresponding membership products is in the order lines
+                return this.allowed_membership_category_ids.some((categ) => {
+                    return (
+                        (partner &&
+                            partner.membership_category_ids.indexOf(categ) !== -1) ||
+                        order.orderlines.some(
+                            (orderline) =>
+                                orderline.product.membership_category_id[0] === categ
+                        )
+                    );
+                });
             }
         };
     Registries.Model.extend(Product, OverloadProduct);
@@ -40,20 +43,17 @@ odoo.define("pos_membership_extension.models", function (require) {
     const OverloadOrder = (OriginalOrder) =>
         class extends OriginalOrder {
             /**
-             * Overloaded function.
-             * Check if the product of the order lines are allowed by the
-             * new selected partner.
+             * Check if the products of the order lines are allowed.
              * If not, remove according lines and raise a PopUp Error to
              * inform the cashier of the removal.
-             * @param {partner} partner to set to the order. (can be undefined)
-             * @returns {Boolean} In any case, return the result of the super function.
              */
-            set_partner(partner) {
+            _remove_non_allowed_orderlines() {
+                var self = this;
                 var bad_product_list = [];
                 var i = this.orderlines.length;
                 while (i--) {
                     var orderline = this.orderlines[i];
-                    if (!orderline.product.get_membership_allowed(partner)) {
+                    if (!orderline.product.get_membership_allowed(self)) {
                         bad_product_list.push(orderline.product.display_name);
                         this.orderlines.splice(i, 1);
                     }
@@ -67,10 +67,28 @@ odoo.define("pos_membership_extension.models", function (require) {
                         ),
                     });
                 }
+            }
 
-                return super.set_partner(...arguments);
+            /**
+             * Overloaded function.
+             * @param {Orderline} line - The order line to be removed.
+             */
+            remove_orderline(line) {
+                super.remove_orderline(line);
+                if (line.product.membership) {
+                    this._remove_non_allowed_orderlines();
+                    this.select_orderline(this.get_last_orderline());
+                }
+            }
+
+            /**
+             * Overloaded function.
+             * @param {partner} partner to set to the order. (can be undefined)
+             */
+            set_partner(partner) {
+                super.set_partner(partner);
+                this._remove_non_allowed_orderlines();
             }
         };
-
     Registries.Model.extend(Order, OverloadOrder);
 });
